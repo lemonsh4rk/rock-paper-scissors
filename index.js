815c4ae3de2e4609177dfa73bed35f06a155599b
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-11 15:33:06 -0300

    feat: add all rock-paper-scissors outcomes
    Now i have to make it work, just one game

--- index.js
+++ index.js
@@ -10,3 +10,36 @@ function getPlayerChoice() {
   let playerChoice = prompt("Choose: Rock - Paper or Scissors!") ;
   return playerChoice.toLocaleLowerCase();
 }
+
+function rockPaperScissors(playerSelection, computerSelection) {
+  console.log(`You played: ${playerSelection}`);
+  console.log(`Computer played: ${computerSelection}`)
+  let gameOver = false;
+  while (!gameOver) {
+    if ((playerSelection === "rock") && (computerSelection === "paper")) {
+      console.log("You lose, good luck next time!")
+      gameOver = true;
+    } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
+      console.log("You lose, good luck next time!")
+      gameOver = true;
+    } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
+      console.log("You lose, good luck next time!")
+      gameOver = true;
+    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+      console.log("You win, congratulations!")
+      gameOver = true;
+    } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+      console.log("You win, congratulations!")
+      gameOver = true;
+    } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+      console.log("You win, congratulations!")
+      gameOver = true;
+    } else if (playerSelection === computerSelection){
+      console.log(`You tied, try again!`);
+    } else {
+      gameOver = true;
+    }
+  }
+}
+
+rockPaperScissors(getPlayerChoice(), getComputerChoice());
