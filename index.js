c40e0deb45b146c41373bbea7d562aae2feacb32
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-11 15:43:22 -0300

    feat: finished rock paper scissors
    It took longer than i expected, and i'm sure there is a better way to do things.
    But i'm proud of this one, outside of the random choice from the computer i got everything on my own which was my goal.
    I don't know if a while loop is the way to go though. I did this exercise once in python, waaay easier syntax. I was thinking of a switch statement or maybe another type of loop. But i generally like the gameOver = false approach. Excercise done :D.

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-function getComputerChoice() {
-let choice = ["rock", "paper", "scissors"];
-
-let random = Math.floor((Math.random() * choice.length));
-
-return choice[random];
-}
-
-function getPlayerChoice() {
-  let playerChoice = prompt("Choose: Rock - Paper or Scissors!") ;
-  return playerChoice.toLocaleLowerCase();
-}
-
-function rockPaperScissors(playerSelection, computerSelection) {
-  console.log(`You played: ${playerSelection}`);
-  console.log(`Computer played: ${computerSelection}`)
-  let gameOver = false;
-  while (!gameOver) {
-    if ((playerSelection === "rock") && (computerSelection === "paper")) {
-      console.log("You lose, good luck next time!")
-      gameOver = true;
-    } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
-      console.log("You lose, good luck next time!")
-      gameOver = true;
-    } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
-      console.log("You lose, good luck next time!")
-      gameOver = true;
-    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-      console.log("You win, congratulations!")
-      gameOver = true;
-    } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-      console.log("You win, congratulations!")
-      gameOver = true;
-    } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-      console.log("You win, congratulations!")
-      gameOver = true;
-    } else if (playerSelection === computerSelection){
-      console.log(`You tied, try again!`);
-    } else {
-      gameOver = true;
-    }
+let gameOver = false;
+while (!gameOver) {
+  function getComputerChoice() {
+    let choice = ["rock", "paper", "scissors"];
+    let random = Math.floor((Math.random() * choice.length));
+    return choice[random];
   }
-}
 
-rockPaperScissors(getPlayerChoice(), getComputerChoice());
+  function getPlayerChoice() {
+      let playerChoice = prompt("Choose: Rock - Paper or Scissors!") ;
+      return playerChoice.toLocaleLowerCase();
+  }
+  
+  function rockPaperScissors(playerSelection, computerSelection) {
+    console.log(`You played: ${playerSelection}`);
+    console.log(`Computer played: ${computerSelection}`)
+  if ((playerSelection === "rock") && (computerSelection === "paper")) {
+    console.log("You lose, good luck next time!")
+    gameOver = true;
+  } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
+    console.log("You lose, good luck next time!")
+    gameOver = true;
+  } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
+    console.log("You lose, good luck next time!")
+    gameOver = true;
+  } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+    console.log("You win, congratulations!")
+    gameOver = true;
+  } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+    console.log("You win, congratulations!")
+    gameOver = true;
+  } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+    console.log("You win, congratulations!")
+    gameOver = true;
+  } else if (playerSelection === computerSelection){
+    console.log(`You tied, try again!`);
+  } else {
+    gameOver = true;
+  }}
+  rockPaperScissors(getPlayerChoice(), getComputerChoice())
+}
