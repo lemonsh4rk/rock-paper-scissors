ff5bb8c109fdeb64b397de317f7bee42057dadb9
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-26 16:26:37 -0300

    feat: add score tracker

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let optionsButtons = document.querySelectorAll("button")
-let resultText = document.querySelector("#resulttext")
-let computerSelectionText = document.querySelector("#computerselectiontext")
+let optionsButtons = document.querySelectorAll("button");
+let resultText = document.querySelector("#resulttext");
+let computerSelectionText = document.querySelector("#computerselectiontext");
+let computerScoreText = document.querySelector("#computerscoretext");
+let playerScoreText = document.querySelector("#playerscoretext");
 
+let gameOver = false;
+let playerScore = 0;
+let computerScore = 0;
 
 let computerSelection;
 let playerSelection;
@@ -18,26 +23,32 @@ function playRound(computerSelection, playerSelection) {
   if ((playerSelection === "ROCK") && (computerSelection === "PAPER")) {
 
     resultText.textContent = `You LOSE! Paper beats rock`;
+    computerScore++;
   
   } else if ((playerSelection === "PAPER") && (computerSelection === "SCISSORS")) {
 
     resultText.textContent = `You LOSE! Scissor beats rock`;
+    computerScore++;
   
   } else if ((playerSelection === "SCISSORS") && (computerSelection === "ROCK")) {
 
     resultText.textContent = `You LOSE! Rock beats scissors`;
+    computerScore++;
   
   } else if ((playerSelection === "ROCK") && (computerSelection === "SCISSORS")) {
 
     resultText.textContent = `You WIN! Rock beats scissors`;
+    playerScore++;
   
   } else if ((playerSelection === "PAPER") && (computerSelection === "ROCK")) {
 
     resultText.textContent = `You WIN! Paper beats rock`;
+    playerScore++;
   
   } else if ((playerSelection === "SCISSORS") && (computerSelection === "PAPER")) {
 
     resultText.textContent = `You WIN! Scissor beats paper`;
+    playerScore++;
   
   } else if (playerSelection === computerSelection){
 
@@ -54,7 +65,11 @@ optionsButtons.forEach(selectionButton => selectionButton.addEventListener("clic
   playerSelection = selectionButton.textContent;
   computerSelection  = getComputerChoice();
 
-  computerSelectionText.textContent = `Computer Played: ${computerSelection}`;
+  computerSelectionText.textContent = `Computer played: ${computerSelection}`;
 
-  playRound(computerSelection, playerSelection)
+  playRound(computerSelection, playerSelection);
+
+  computerScoreText.textContent = `Computer score: ${computerScore}`;
+  playerScoreText.textContent = `Player score: ${playerScore}`;
+
 }))
